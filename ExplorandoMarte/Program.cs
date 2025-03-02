34b0744f786cec3214c14917b6ec97fd33d91b77
João Fernando <joao_jfmx@outlook.com>
2025-03-02 17:32:11 -0300

    RELEASE 1.0

--- ExplorandoMarte/Program.cs
+++ ExplorandoMarte/Program.cs
@@ -121,11 +121,6 @@ namespace ExplorandoMarte
                     rovers.Add(rover);
                 }
 
-                // Output the final position and Direction of each rover
-                foreach (var rover in rovers)
-                {
-                    Console.WriteLine(rover.GetPosition());
-                }
             }
             catch (Exception ex)
             {
